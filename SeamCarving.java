7b2a47c16970c31f02cd73c191aa4d5f0ee9fb1e
Angie Chiang <angieyayabird@gmail.com>
2014-08-27 23:12:51 -0400

    add column seam carving

--- SeamCarving.java
+++ SeamCarving.java
@@ -4,18 +4,24 @@ import java.io.*;
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import static java.lang.Math.*;
 
-class Pixel extends Color {
+
+class Pixel {
     Pixel up;
     Pixel down;
     Pixel left;
     Pixel right;
+    Color c;
+    int e;
+    int dp;
+    Pixel bt;
 
     public Pixel(){
-        super(0);
+        c = new Color(0);
     }
     public Pixel(int rgb){
-        super(rgb);
+        c = new Color(rgb);
     }
     static public Pixel img2graph(BufferedImage img){
         int width = img.getWidth();
@@ -88,10 +94,78 @@ class Pixel extends Color {
                 if(w > 0){
                     pw = pw.right;
                 }
-                img.setRGB(w, h, pw.getRGB());
+                img.setRGB(w, h, pw.c.getRGB());
             }
         }
         return img;
+    }
+    static public int absGrad(Pixel a, Pixel b){
+        return abs(a.c.getRed()   - b.c.getRed())   + 
+               abs(a.c.getGreen() - b.c.getGreen()) + 
+               abs(a.c.getBlue()  - b.c.getBlue());
+    }
+    static public void calEnergy(Pixel root){
+        Pixel ph = root;
+        Pixel pw = root;
+        while(ph != null){
+            pw = ph;
+            while(pw != null){
+                if(pw.left != null){
+                    pw.e += absGrad(pw, pw.left);
+                }
+                else if(pw.right != null){
+                    pw.e += absGrad(pw, pw.right);
+                }
+
+                if(pw.up != null){
+                    pw.e += absGrad(pw, pw.up);
+                }
+                else if(pw.down != null){
+                    pw.e += absGrad(pw, pw.down);
+                }
+
+                if(pw.up != null){
+                    pw.dp = pw.up.dp;
+                    pw.bt = pw.up;
+                    if(pw.up.left != null && pw.up.left.dp < pw.dp){
+                        pw.dp = pw.up.left.dp;
+                        pw.bt = pw.up.left;
+                    }
+                    if(pw.up.right != null && pw.up.right.dp < pw.dp){
+                        pw.dp = pw.up.right.dp;
+                        pw.bt = pw.up.right;
+                    }
+                }
+                pw.dp += pw.e;
+                pw = pw.right;
+            }
+            ph = ph.down;
+        }
+    }
+    static public void mkColumn(Pixel root){
+        Pixel p = root;
+        // find the bottom left pixel
+        while(p.down != null){
+            p = p.down;
+        }
+
+        // find the minimum end
+        Pixel min = p;
+        int i = 0;
+        while(p != null){
+            if(p.dp < min.dp){
+                min = p;
+            }
+            p = p.right;
+        }
+
+        // back tracking
+        Pixel curr = min;
+        while(curr != null){
+            System.out.println("bt");
+            curr.c = new Color(255, 0, 0);
+            curr = curr.bt;
+        }
     }
 }
 
@@ -109,6 +183,8 @@ class SeamCarving extends JPanel{
         catch (Exception e){}
 
         Pixel root = Pixel.img2graph(img);
+        Pixel.calEnergy(root);
+        Pixel.mkColumn(root);
         BufferedImage img2 = Pixel.graph2img(root);
         return img2;
     }
